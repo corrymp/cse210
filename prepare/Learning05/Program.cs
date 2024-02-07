c1723e78a5c37c28e80adadc4f8f933de53b64b2
corrymp <corrympalmer@gmail.com>
2024-02-06 18:30:13 -0800

    W05 - Shapes Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new()
+        {
+            new Square("Red",3),
+            new Rectangle("Green",2,3),
+            new Circle("Blue",2)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} | {shape.GetArea()}");
+        }
     }
 }
